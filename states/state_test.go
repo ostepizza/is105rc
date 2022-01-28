604ff9b1f6cea2c9626187b2a7cff4b51cdd3cf6
ostepizza <markus@nils1.net>
2022-01-28 23:26:41 +0100

    test

--- states/state_test.go
+++ states/state_test.go
@@ -4,9 +4,9 @@ package state
 import "testing"
 
 // Test function implemented in line with the Golang's testing tool
-func TestViewState(t *testing.T) {
-    wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
-    state := ViewState();
+func test(t *testing.T) {
+    wanted := "\\_Farmer_"*"_/" || "\\_"*"_Farmer_/"
+    state := boat
     if state != wanted {
          t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
     }
